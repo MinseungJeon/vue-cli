b03164cafe5fa3ddb9bc22ad4a56a4e2e348f39f
전민승 <jeon@jeonminseungs-MacBook-Pro.local>
2021-02-13 18:47:04 +0900

    Introduction Components

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,24 @@
 <template>
   <div id="app">
-    <h1>{{ msg }}</h1>
+    <my-cmp></my-cmp>
+    <hr>
+    <my-cmp></my-cmp>
   </div>
 </template>
 
 <script>
-export default {
-  data () {
-    return {
-      // note: changing this line won't causes changes
-      // with hot-reload because the reloaded component
-      // preserves its current state and we are modifying
-      // its initial state.
-      msg: 'Hello Vue!'
+Vue.component('my-cmp', {
+  data: function(){
+    return {status: 'Critical'
     }
-  }
-}
+  },
+  template: '<p>Server Status: {{ status }}</p>'
+});
+
+new Vue({
+  el: '#app'
+})
 </script>
 
 <style>
-body {
-  font-family: Helvetica, sans-serif;
-}
 </style>
